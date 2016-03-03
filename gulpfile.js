e04b662c197bef23bdfbb90e411c82e6fca2090b
Alan Souza <alansouzati@gmail.com>
2016-03-02 16:33:16 -0800

    Added travis integration for the blog.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp');
-var path = require('path');
 var argv = require('argv');
+var del = require('del');
 var devGulpTasks = require('grommet/utils/gulp/gulp-tasks');
+var git = require('gulp-git');
+var gulp = require('gulp');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
 var opts = {
   copyAssets: [
@@ -32,13 +35,81 @@ var opts = {
   },
   devPreprocess: ['set-webpack-alias'],
   scsslint: true,
-  customEslintPath: path.resolve(__dirname, 'customEslintrc'),
+  customEslintPath: path.resolve(__dirname, 'customEslintrc')
 };
 
 gulp.task('set-webpack-alias', function () {
   if (opts.alias && argv.useAlias) {
     console.log('Using local alias for development.');
     opts.webpack.resolve.alias = opts.alias;
+  }
+});
+
+gulp.task('release:createTmp', function(done) {
+  del.sync(['./tmp']);
+  mkdirp('./tmp', function(err) {
+    if (err) {
+      throw err;
+    }
+    done();
+  });
+});
+
+gulp.task('release:heroku', ['dist', 'release:createTmp'], function(done) {
+  if (process.env.CI) {
+    git.clone('https://' + process.env.GH_TOKEN + '@github.com/grommet/grommet-blog.git',
+      {
+        cwd: './tmp/'
+      },
+      function(err) {
+        if (err) {
+          throw err;
+        }
+
+        process.chdir('./tmp/grommet-blog');
+        git.checkout('heroku', function(err) {
+          if (err) {
+            throw err;
+          }
+
+          del.sync(['./**/*']);
+
+          gulp.src('../../dist/**').pipe(gulp.dest('./')).on('end', function() {
+            git.status({
+              args: '--porcelain'
+            }, function(err, stdout) {
+              if (err) {
+                throw err;
+              }
+
+              if (stdout && stdout !== '') {
+                gulp.src('./')
+                  .pipe(git.add({
+                    args: '--all'
+                  }))
+                  .pipe(git.commit('Heroku version update.')).on('end', function() {
+                    git.push('origin', 'stable', { quiet: true }, function(err) {
+                      if (err) {
+                        throw err;
+                      }
+
+                      process.chdir(__dirname);
+                      done();
+                    });
+                  });
+              } else {
+                console.log('No difference since last commit, skipping heroku release.');
+
+                process.chdir(__dirname);
+                done();
+              }
+            });
+          });
+        });
+      }
+    );
+  } else {
+    console.warn('Skipping release. Release:heroku task should be executed by CI only.');
   }
 });
 
