852b513e4a532de590a1880708c5b77fe93e9109
Alan Souza <alansouzati@gmail.com>
2016-05-18 18:11:59 -0700

    Update blog.js

--- server/blog.js
+++ server/blog.js
@@ -36,14 +36,6 @@ const USER_PASSWORD = process.env.USER_PASSWORD || 'admin';
 const auth = basicAuth(USER, USER_PASSWORD);
 
 const app = express();
-function requireHTTPS(req, res, next) {
-  if (!req.secure && !SKIP_SSL) {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
-app.use(requireHTTPS);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(compression());
@@ -69,6 +61,8 @@ function routerProcessor (req, res, next) {
     match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
       if (error) {
         res.status(500).send(error.message);
+      } else if (!req.secure && !SKIP_SSL) {
+        res.redirect(302, 'https://' + req.get('host') + req.url);
       } else if (redirectLocation) {
         res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       } else if (renderProps) {
