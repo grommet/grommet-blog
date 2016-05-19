bc1ca501d39c4c9e4df729e9485f19c06ec6bcb5
Alan Souza <alansouzati@gmail.com>
2016-05-18 18:16:16 -0700

    Update blog.js

--- server/blog.js
+++ server/blog.js
@@ -44,7 +44,9 @@ app.use(bodyParser.json());
 app.use(busboyBodyParser());
 
 function routerProcessor (req, res, next) {
-  if (/\..*$/.test(req.url)) {
+  if (!req.secure && !SKIP_SSL) {
+    res.redirect(302, 'https://' + req.get('host') + req.url);
+  } else if (/\..*$/.test(req.url)) {
     next();
   } else if (/^\/\d{4}\/\d{2}\/\d{2}\/.*/.test(req.url)) {
     // necessary for the old posts not following our regex structure
@@ -61,8 +63,6 @@ function routerProcessor (req, res, next) {
     match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
       if (error) {
         res.status(500).send(error.message);
-      } else if (!req.secure && !SKIP_SSL) {
-        res.redirect(302, 'https://' + req.get('host') + req.url);
       } else if (redirectLocation) {
         res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       } else if (renderProps) {
